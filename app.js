a59e41fd59df360b065435df991d00c1c747bdab
Steve Rector <steverector27@gmail.com>
2024-02-04 11:04:53 -0800

    Add port to env file, sets up epxress to use json

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-import "dotenv/config";
+"use strict";
 
-console.log("Choo chic");
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// root of app
+app.get("/", (req, res) => {
+  res.send("suh dudeeeee!");
+});
+
+// sets app to listen to localhost 3000
+app.listen(process.env.PORT, () =>
+  console.log(`app listening on ${process.env.PORT}`)
+);
