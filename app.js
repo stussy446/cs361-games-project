c849ad35bba620a7c05b759f8203236d86149aaf
Steve Rector <steverector27@gmail.com>
2024-02-05 12:24:47 -0800

    Add logic to show recent games on the home page

--- app.js
+++ app.js
@@ -25,8 +25,13 @@ app.set("view engine", "ejs");
 
 // home page
 app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Root of project",
+  let query1 = `SELECT * FROM games ORDER BY idgames DESC LIMIT 3;`;
+
+  pool.query(query1, function (error, rows, fields) {
+    res.render("index", {
+      title: "Root of project",
+      games: rows,
+    });
   });
 });
 
