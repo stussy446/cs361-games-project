6888e7d92cad44931f7bb50442ec4395c93c2150
Steve Rector <steverector27@gmail.com>
2024-02-05 13:54:05 -0800

    Add layout and navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import expressEjsLayouts from "express-ejs-layouts";
 
 import { default as gameRouter } from "./src/routes/Games.js";
 import { default as homeRouter } from "./src/routes/Index.js";
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(expressEjsLayouts);
 
 // route configuration
 app.use("/", homeRouter);
